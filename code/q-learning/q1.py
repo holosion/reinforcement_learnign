1990f957d3bae23f807c1443724409bf89af789c
Akena Jonathana <akenajonathan24@gmail.com>
2026-09-13 10:59:49 +0300

    Implement episode loop for environment interaction and scoring

--- code/q-learning/q1.py
+++ code/q-learning/q1.py
@@ -8,6 +8,19 @@ from stable_baselines3.common.evaluation import evaluate_policy
 
 
 # loading up the environment
+env = gym.make('CartPole-v1')
+episodes = 5
+for episode in range(1, episodes):
+    state = env.reset()
+    done = False
+    score = 0
+
+    while not done:
+        env.render()
+        action = env.action_space.sample()
+        n_state, reward, done, info = env.step(action)
+        score += reward
+        print('Episode{} score{}'.format(episode,score))
 
 
 
